941545eaf73464ce379d5aaa20b7b51ce40038a8
Dj <ritchey0713@gmail.com>
2020-02-25 17:44:23 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace textFileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

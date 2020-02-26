e76bb400f4a1379fb4648c0c401333ef38a3d44d
Dj <ritchey0713@gmail.com>
2020-02-25 21:43:13 -0500

    read from text files

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@
 namespace textFileApp {
   class Program {
   	static void Main(string[] args){
-      
+       string text = System.IO.File.ReadAllText(@"/home/maveryk/Desktop/code/hello.txt");
+
+			 System.Console.WriteLine("Text file contains: {0}", text);
     }
   }
 }

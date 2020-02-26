f05f79203b1a65c33d90a6fa543eb359e7b5f2f2
Dj <ritchey0713@gmail.com>
2020-02-25 21:48:01 -0500

    write to new file

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.IO;
 
 namespace textFileApp {
   class Program {
   	static void Main(string[] args){
-       string text = System.IO.File.ReadAllText(@"/home/maveryk/Desktop/code/hello.txt");
 
-			 System.Console.WriteLine("Text file contains: {0}", text);
+		// 	// ex 1 read text
+    //    string text = System.IO.File.ReadAllText(@"/home/maveryk/Desktop/code/hello.txt");
+
+		// 	 System.Console.WriteLine("Text file contains: {0}", text);
+
+		// 	// get array of lines
+		// 	 string[] lines = System.IO.File.ReadAllLines(@"/home/maveryk/Desktop/code/hello.txt");
+
+		// 	 foreach(string line in lines){
+		// 		 System.Console.WriteLine(line);
+		// 	 }
+
+			string[] lines = {"First line", "Second Line", "Third line!"};
+
+			
+
+			File.WriteAllLines(@"/home/maveryk/Desktop/code/hello2.txt", lines);
+
     }
   }
 }
